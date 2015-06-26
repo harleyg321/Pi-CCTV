b40ce1d75a617fee2da85d51ed4118c0140af4e4
Harley Glossop <harleyglossop@gmail.com>
2015-06-26 18:45:01 +0100

    updated to use avconv and be more streamlined

--- housekeeping.py
+++ housekeeping.py
@@ -5,23 +5,14 @@ import glob
 import time
 import shutil
 
-RAW = "/home/pi/cctv/raw"
-FINAL = "/mnt/share/cctv"
+SHARE = "/media/share/cctv"
 
 def check_drive():
 	return os.path.exists("/mnt/share/cctv/DO_NOT_DELETE")
-		#time.sleep(60)
-		#subprocess.Popen("/usr/bin/sudo /sbin/shutdown -r now", shell=True)
-		#sys.exit()
-	#return
 
-def pi_free_space():
-	pi_drive = os.statvfs(RAW)
-	return pi_drive.f_bavail * pi_drive.f_frsize / 1024 / 1024
-
-def store_free_space():
-	store_drive = os.statvfs(FINAL)
-	return store_drive.f_bavail * store_drive.f_frsize / 1024 / 1024
+def free_space():
+	drive = os.statvfs(SHARE)
+	return drive.f_bavail * drive.f_frsize / 1024 / 1024
 
 def folder_listing(path):
 	old_path = os.getcwd()
@@ -31,18 +22,11 @@ def folder_listing(path):
 	return folders
 
 while True:
-	if(pi_free_space() < 5000):
-		#pi is low on space (unlikely, means NAS is not accessible)
-		pi_folders = folder_listing(RAW)
-		if len(pi_folders) > 0:
-			shutil.rmtree(RAW + "/" + pi_folders[0])
-
 	if check_drive():	
-		if(store_free_space() < 5000):
+		if(free_space() < 5000):
 			#NAS is low on space, delete a day
-			store_folders = folder_listing(FINAL)
-			if len(store_folders) > 0:
-				shutil.rmtree(FINAL + "/" + store_folders[0])
-				shutil.rmtree("/mnt/share/trashbox/cctv")
-
+			folders = folder_listing(SHARE)
+			if len(folders) > 0:
+				shutil.rmtree(SHARE + "/" + store_folders[0])
+				shutil.rmtree("/media/share/trashbox/cctv")
 	time.sleep(60)
--- record.py
+++ record.py
@@ -1,86 +1,87 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import picamera
-import datetime
 import subprocess
-import errno
-import sys
+import datetime
 import os
 import time
-import threading
-import Queue
+import errno
+import collections
+
+SHARE = "/media/share/cctv"
+DEVNULL = open(os.devnull, 'w')
+WIDTH = 1920
+HEIGHT = 1080
+FRAMERATE = 30
 
 def create_folder(time):
 	check_drive()
 	try:
-		os.makedirs("/home/pi/cctv/raw/{}".format(time.strftime('%Y-%m-%d')))
+		os.makedirs(SHARE + "/{}".format(time.strftime('%Y-%m-%d')))
 	except OSError as exception:
 		if exception.errno != errno.EEXIST:
 			raise
 	return
 
 def check_drive():
-	while not os.path.exists("/mnt/share/cctv/DO_NOT_DELETE"):
+	while not os.path.exists(SHARE + "/DO_NOT_DELETE"):
 		time.sleep(60)
 	return
-		#time.sleep(60)
-		#subprocess.Popen("/usr/bin/sudo /sbin/shutdown -r now", shell=True)
-		#sys.exit()
-	#return
 
-def capture_image(camera, capture):
-	while True:
-		cap = capture.get()
-		if cap:
-			camera.capture("/home/pi/cctv/public_html/cctv.jpg", use_video_port=True, splitter_port=2)
-			capture.put(False)
+class Output:
+
+	def __init__(self, camera):
+		self.camera = camera
+		self.camera.resolution = (WIDTH, HEIGHT)
+		self.camera.framerate = FRAMERATE
+		self.running = False
+		self.process = collections.deque([],maxlen=3)
+
+	def record(self, now):
+		self.setProcess(subprocess.Popen("/usr/bin/avconv -f h264 -r " + str(FRAMERATE) + " -i pipe: -vcodec copy -vsync passthrough -r " + str(FRAMERATE) + " " + SHARE + "/{}/{}.mp4".format(now.strftime('%Y-%m-%d'),now.strftime('%H-%M-%S')), stdin=subprocess.PIPE, stderr=DEVNULL, bufsize=0, shell=True))
+		if self.running:
+			self.camera.split_recording(self.getstdin())
+			self.closestdin()
 		else:
-			time.sleep(0.1)
+			self.camera.start_recording(self.getstdin(), format='h264')
+			self.running = True
 
-if __name__ == '__main__':
-	with picamera.PiCamera() as camera:
-		capture = Queue.Queue()
-		capture.put(False)
-		t = threading.Thread(target=capture_image, args=(camera,capture))
-		t.start()
 
-		camera.resolution = (1920, 1080)
-		camera.framerate = 30
-		camera.annotate_text = datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')
-		now = datetime.datetime.now()	
-		create_folder(now)
-		try:
-			camera.start_recording('/home/pi/cctv/raw/{}/{}.h264'.format(now.strftime('%Y-%m-%d'),now.strftime('%H-%M-%S')),bitrate=5000000)
-			print("{}/{}".format(now.strftime('%Y-%m-%d'),now.strftime('%H-%M-%S')))
-		except:
-			subprocess.Popen("/usr/bin/sudo /sbin/shutdown -r now", shell=True)
-			sys.exit()
-		while True:
-			start = datetime.datetime.now()
-			last = start
-			while (datetime.datetime.now() - start).seconds < 598:
-				dt = datetime.datetime.now()
-				camera.annotate_text = dt.strftime('%H:%M:%S %d/%m/%Y')
-				if (dt-last).seconds > 0:
-					capture.put(True)
-					last = dt
-				else:
-					camera.wait_recording(0.1)
-			now = datetime.datetime.now()
-			create_folder(now)
-			try:
-				camera.split_recording('/home/pi/cctv/raw/{}/{}.h264'.format(now.strftime('%Y-%m-%d'),now.strftime('%H-%M-%S')))
-				print("{}/{}".format(now.strftime('%Y-%m-%d'),now.strftime('%H-%M-%S')))
-			except:
-				try:
-					camera.stop_recording()
-					os.remove('/home/pi/cctv/raw/{}/{}.h264'.format(now.strftime('%Y-%m-%d'),now.strftime('%H-%M-%S')))
-				except:
-					pass
-				try:
-					camera.start_recording('/home/pi/cctv/raw/{}/{}.h264'.format(now.strftime('%Y-%m-%d'),now.strftime('%H-%M-%S')),bitrate=5000000)
-					print("{}/{}".format(now.strftime('%Y-%m-%d'),now.strftime('%H-%M-%S')))
-				except:
-					subprocess.Popen("/usr/bin/sudo /sbin/shutdown -r now", shell=True)
-					sys.exit()
-		camera.stop_recording()
+	def getstdin(self):
+		return self.getProcess().stdin
+
+	def closestdin(self):
+		if len(self.process)>1:
+			self.getProcess(True).stdin.close()
+			return
+
+	def wait(self):
+		self.getProcess().wait()
+		return
+
+	def setProcess(self, process):
+		self.process.append(process)
+		return
+
+	def getProcess(self, previous=False):
+		if previous:
+			return self.process[-2]
+		else:
+			return self.process[-1]
+
+check_drive()
+with picamera.PiCamera() as camera:
+	now = datetime.datetime.now()
+	create_folder(now)
+	camera.annotate_text = now.strftime('%H:%M:%S %d/%m/%Y')
+	output = Output(camera)
+	output.record(now)
+	while True:
+		start = datetime.datetime.now()
+		while(datetime.datetime.now() - start).seconds < 3600:
+			camera.annotate_text = datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')
+			camera.wait_recording(0.1)
+		output.record(datetime.datetime.now())
+	camera.stop_recording()
+	output.getstdin().close()
+	output.wait()
